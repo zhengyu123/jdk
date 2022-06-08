0f2213683be345f6301d65bc3228396b4da7075a
Zhengyu Gu <zgu@redhat.com>
2022-06-08 13:57:40 -0400

    8288049: Win32 build failed

--- src/hotspot/share/code/codeBlob.hpp
+++ src/hotspot/share/code/codeBlob.hpp
@@ -510,6 +510,7 @@ class RuntimeStub: public RuntimeBlob {
   // below two-argument operator delete will be treated as a placement
   // delete rather than an ordinary sized delete; see C++14 3.7.4.2/p2.
   void operator delete(void* p);
+  void operator delete (void* p, size_t size);
   void* operator new(size_t s, unsigned size) throw();
 
  public:
@@ -547,10 +548,6 @@ class SingletonBlob: public RuntimeBlob {
   friend class VMStructs;
 
  protected:
-  // This ordinary operator delete is needed even though not used, so the
-  // below two-argument operator delete will be treated as a placement
-  // delete rather than an ordinary sized delete; see C++14 3.7.4.2/p2.
-  void operator delete(void* p);
   void* operator new(size_t s, unsigned size) throw();
 
  public:
@@ -767,6 +764,10 @@ class UpcallStub: public RuntimeBlob {
                      intptr_t exception_handler_offset,
                      jobject receiver, ByteSize frame_data_offset);
 
+  // This ordinary operator delete is needed even though not used, so the
+  // below two-argument operator delete will be treated as a placement
+  // delete rather than an ordinary sized delete; see C++14 3.7.4.2/p2.
+  void operator delete(void* p);
   void* operator new(size_t s, unsigned size) throw();
 
   struct FrameData {
